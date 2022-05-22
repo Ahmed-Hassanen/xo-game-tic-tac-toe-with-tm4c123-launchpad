a560f6c708199f617247a02fc1388d395ac1ef2d
Ahmed_Hassanen <ahmed180116@feng.bu.edu.eg>
2022-05-22 08:30:14 +0200

    xo game

---
+++ SpaceInvaders.c
@@ -0,0 +1,376 @@
+#include "tm4c123gh6pm.h"
+#include "Nokia5110.h"
+#include "Random.h"
+#include "TExaS.h"
+
+// Blue Nokia 5110
+// ---------------
+// Signal        (Nokia 5110) LaunchPad pin
+// Reset         (RST, pin 1) connected to PA7
+// SSI0Fss       (CE,  pin 2) connected to PA3
+// Data/Command  (DC,  pin 3) connected to PA6
+// SSI0Tx        (Din, pin 4) connected to PA5
+// SSI0Clk       (Clk, pin 5) connected to PA2
+// 3.3V          (Vcc, pin 6) power
+// back light    (BL,  pin 7) not connected, consists of 4 white LEDs which draw ~80mA total
+// Ground        (Gnd, pin 8) ground
+
+void draw(void);
+void reset(void);
+void Timer2_Init(unsigned long  period);
+void DisableInterrupts(void); // Disable interrupts
+void EnableInterrupts(void);  // Enable interrupts
+void PortE_Init(void);        
+void PortF_Init(void);
+int moves=0;
+void theWinnerIs(void);
+int delay;
+void Delay100ms(unsigned long count);
+
+char array[10]={'a',' ',' ',' ',' ',' ',' ',' ',' ',' '};
+
+
+void WaitForInterrupt(void);
+int i =0,j=0,indx = 1 , current_cursor = 1;
+char turn = 'X';
+char x_id='X';
+char o_id='O';
+int x_score=0;
+int o_score=0;
+volatile int cursor=1;
+
+void PortE_Init(void){
+	SYSCTL_RCGC2_R  |= (1<<4);        //port E clock
+  delay = 0;                        // delay   
+  GPIO_PORTE_LOCK_R = 0x4C4F434B;   // 2) unlock PortE
+  GPIO_PORTE_CR_R = (1<<0)|(1<<1);           // allow changes to PE0,PE1    
+  GPIO_PORTE_AMSEL_R = 0x00;        //  disable analog function
+  GPIO_PORTE_PCTL_R = 0x00000000;   // GPIO clear bit PCTL  
+  GPIO_PORTE_DIR_R = 0x1;          //  PE0 OUTPUT PE1 INPUT 
+  GPIO_PORTE_AFSEL_R = 0x00;        //  no alternate function
+  GPIO_PORTE_DEN_R =0x1F;        //  enable digital pins PE0,PE1  
+GPIO_PORTF_PUR_R |= 0x1;  	   //enable pullup resistors on PE1
+ GPIO_PORTE_IS_R &= ~(1<<1);             //EDGE SENSE
+	GPIO_PORTE_IBE_R &= ~(1<<1);            //SINGLE EDGE
+	GPIO_PORTE_IEV_R |= (1<<1);              //RAISING EDGE
+	GPIO_PORTE_ICR_R = (1<<1);     // CLEAR FLAGS ON PE1
+	GPIO_PORTE_IM_R |= (1<<1);			// ARM INTERRUPT PE1
+	NVIC_PRI1_R =(5<<5);             //PRIORITY 5
+NVIC_EN0_R =(1<<4);          //ENABLE INTERRUPT 4
+EnableInterrupts();
+	 
+}
+
+void PortF_Init(void){                          
+                        
+   SYSCTL_RCGC2_R |= 0x00000020;     // 1) F clock
+  delay = SYSCTL_RCGC2_R;           // delay   
+  GPIO_PORTF_LOCK_R = 0x4C4F434B;   // 2) unlock PortF PF0  
+  GPIO_PORTF_CR_R = 0x1F;           // allow changes to PF4-0       
+  GPIO_PORTF_AMSEL_R = 0x00;        // 3) disable analog function
+  GPIO_PORTF_PCTL_R = 0x00000000;   // 4) GPIO clear bit PCTL  
+  GPIO_PORTF_DIR_R = 0x0E;          // 5) PF4,PF0 input, PF3,PF2,PF1 output   
+  GPIO_PORTF_AFSEL_R = 0x00;        // 6) no alternate function
+  GPIO_PORTF_DEN_R = 0x1F;          // 7) enable digital pins PF4-PF0       
+	GPIO_PORTF_PUR_R |= 0x11;          // enable pullup resistors on PF4,PF0       
+  GPIO_PORTF_IS_R &= ~0x11;        //EDGE SENSE
+  GPIO_PORTF_IBE_R &= ~0x11;       //SINGLE EDGE
+  GPIO_PORTF_IEV_R &= ~0x11;          //FAILING EDGE
+  GPIO_PORTF_ICR_R = 0x11;          // CLEAR FLAGS ON PF0,PF4
+  GPIO_PORTF_IM_R |= 0x11;     // ARM INTERRUPT PF0,PF4
+  NVIC_PRI7_R = (NVIC_PRI7_R&0xFF00FFFF)|0x00A00000;  //PRIORITY 5
+  NVIC_EN0_R = 0x40000000;          //ENABLE INTERRUPT 30
+ EnableInterrupts();          
+
+}
+
+
+
+void GPIOPortE_Handler (void){
+	
+	if(GPIO_PORTE_RIS_R &(1<<1)){
+	
+		GPIO_PORTE_ICR_R =(1<<1);
+		 
+		if(array[cursor]==' '){
+		if (turn =='X'){
+			array[cursor]='X';
+			turn='O';
+		}
+		else{ 
+			array[cursor]='O';
+		  turn='X';
+		
+		}
+			 draw();
+		moves++;
+		
+		theWinnerIs();
+		
+	}
+		
+}
+
+}
+
+
+void GPIOPortF_Handler(void)
+	{
+	if (GPIO_PORTF_RIS_R &(1<<4))  
+	{
+		Delay100ms(5);
+		
+	
+		GPIO_PORTF_ICR_R|= (1<<4);
+
+		cursor--;
+		while(array[cursor]!=' '){
+			
+		cursor--;
+		if(cursor<1) cursor=9;
+		}
+		
+		draw();
+		
+		
+	}
+	 else if(GPIO_PORTF_RIS_R &(1<<0)){
+				
+Delay100ms(5);
+		 GPIO_PORTF_ICR_R |= (1<<0);
+		 cursor++;
+		while(array[cursor]!=' '){
+			
+			cursor++;
+		if(cursor>9) cursor=1;
+		}
+		
+		draw();
+		
+		
+	}
+		
+	}
+	
+
+
+void draw(void){
+	
+	 Nokia5110_ClearBuffer();
+	Nokia5110_DisplayBuffer();
+	current_cursor=1,indx=1;
+	// Horizontal borders
+for(i = 0 ;i < 8 ;i++){
+		Nokia5110_SetCursor(i, 1);
+		Nokia5110_OutString("-");
+		Nokia5110_SetCursor(i, 3);
+		Nokia5110_OutString("-");
+	}
+
+// Vertical borders
+for(j = 0 ;j < 5 ;j+=2){
+	Nokia5110_SetCursor(2, j);
+	Nokia5110_OutString("|");
+	Nokia5110_SetCursor(5, j);
+	Nokia5110_OutString("|");	
+	}
+
+// arrayay values
+ for(i = 0 ;i < 5;i+=2) {
+	for(j = 0 ;j < 8 ;j+=3){
+			Nokia5110_SetCursor(j, i);
+			Nokia5110_OutChar(array[indx++]);
+	}
+}
+ 
+// Displaying cursor
+ for(i = 0 ;i < 5;i+=2) {
+	for(j = 1 ;j < 8 ;j+=3){
+		if(current_cursor == cursor){
+			Nokia5110_SetCursor(j, i);
+			Nokia5110_OutChar('.');
+		}
+		current_cursor++;
+		}
+}
+
+// 	Display Trun
+			Nokia5110_SetCursor(8,0);
+			Nokia5110_OutString("Turn");
+			Nokia5110_SetCursor(9,1);
+			Nokia5110_OutChar(turn);
+
+
+}
+
+void reset(void){
+	int i;
+	array[0]='a';
+	for(i=1;i<10;i++){
+		array[i]=' ';
+		
+	}
+	turn='X';
+	moves=0;
+	cursor=1;
+	GPIO_PORTE_DATA_R &=~ (1<<0);
+	draw();
+
+}
+
+
+void theWinnerIs(void){//check if there is a winner
+	
+	
+	
+	
+
+	/*
+		to win you should have x or o in
+	0,1,2 or 3,4,5 or 6,7,8 or
+	0,3,6 or 1,4,7 or 2,5,8 or
+	0,4,8 or 2,4,6
+	*/
+
+	//check if X win`
+	if((array[1] == x_id && array[2] == x_id && array[3] == x_id) ||
+		 (array[4] == x_id && array[5] == x_id && array[6] == x_id) ||
+		 (array[7] == x_id && array[8] == x_id && array[9] == x_id) ||
+
+		 (array[1] == x_id && array[4] == x_id && array[7] == x_id) ||
+		 (array[2] == x_id && array[5] == x_id && array[8] == x_id) ||
+		 (array[3] == x_id && array[6] == x_id && array[9] == x_id) ||
+
+	   (array[1] == x_id && array[5] == x_id && array[9] == x_id) ||
+		 (array[3] == x_id && array[5] == x_id && array[7] == x_id) 		){
+			  Nokia5110_ClearBuffer();
+	Nokia5110_DisplayBuffer();
+			 Nokia5110_SetCursor(0,1);
+			Nokia5110_OutString("winner is X");
+			 x_score++;
+			 Nokia5110_SetCursor(0,2);
+	Nokia5110_OutString("X:");
+	Nokia5110_SetCursor(1,2);
+	Nokia5110_OutUDec(x_score);
+	 
+	Nokia5110_SetCursor(0,3);
+	Nokia5110_OutString("O:");
+	Nokia5110_SetCursor(1,3);
+	Nokia5110_OutUDec(o_score);
+			 
+			 GPIO_PORTE_DATA_R |=(1<<0);
+//				Delay100ms(10);
+	Timer2_Init(240000000);
+	 WaitForInterrupt();
+		//	 reset();
+		 }
+	//check if O win`
+	else if((array[1] == o_id && array[2] == o_id && array[3] == o_id) ||
+		 (array[4] == o_id && array[5] == o_id && array[6] == o_id) ||
+		 (array[7] == o_id && array[8] == o_id && array[9] == o_id) ||
+
+		 (array[1] == o_id && array[4] == o_id && array[7] == o_id) ||
+		 (array[2] == o_id && array[5] == o_id && array[8] == o_id) ||
+		 (array[3] == o_id && array[6] == o_id && array[9] == o_id) ||
+
+	   (array[1] == o_id && array[5] == o_id && array[9] == o_id) ||
+		 (array[3] == o_id && array[5] == o_id && array[7] == o_id) 		)
+{
+
+			  Nokia5110_ClearBuffer();
+	Nokia5110_DisplayBuffer();
+			 Nokia5110_SetCursor(0,1);
+			Nokia5110_OutString("winner is O");
+	o_score++;
+	Nokia5110_SetCursor(0,2);
+Nokia5110_OutString("X:");
+	Nokia5110_SetCursor(1,2);
+	Nokia5110_OutUDec(x_score);
+	 
+	Nokia5110_SetCursor(0,3);
+	Nokia5110_OutString("O:");
+	Nokia5110_SetCursor(1,3);
+	Nokia5110_OutUDec(o_score);
+	
+	 GPIO_PORTE_DATA_R |=(1<<0);
+//	Delay100ms(10);
+	Timer2_Init(240000000);
+	 WaitForInterrupt();
+		//	 reset();
+}
+else if(moves==9){
+ Nokia5110_ClearBuffer();
+	Nokia5110_DisplayBuffer();
+			 Nokia5110_SetCursor(3,1);
+			Nokia5110_OutString("DRAW");
+	Nokia5110_SetCursor(0,2);
+Nokia5110_OutString("X:");
+	Nokia5110_SetCursor(1,2);
+	Nokia5110_OutUDec(x_score);
+	 
+	Nokia5110_SetCursor(0,3);
+	Nokia5110_OutString("O:");
+	Nokia5110_SetCursor(1,3);
+	Nokia5110_OutUDec(o_score);
+	GPIO_PORTE_DATA_R |=(1<<0);
+//		Delay100ms(10);
+Timer2_Init(240000000);
+	 WaitForInterrupt();
+	//reset();
+}
+}
+
+
+
+
+int main(void){
+ 
+  TExaS_Init(NoLCD_NoScope);  // set system clock to 80 MHz
+	 Random_Init(1);
+	PortF_Init();
+ 	PortE_Init(); 
+	Nokia5110_Init();
+ draw();
+EnableInterrupts();
+  while(1){
+		WaitForInterrupt();
+	}
+
+}
+
+
+void Delay100ms(unsigned long count){unsigned long volatile time;
+  while(count>0){
+    time = 727240;  // 0.1sec at 80 MHz
+    while(time){
+	  	time--;
+    }
+    count--;
+  }
+}
+void Timer2_Init(unsigned long  period){ 
+  unsigned long volatile delay;
+  SYSCTL_RCGCTIMER_R |= 0x04;   // 0) activate timer2
+  delay = SYSCTL_RCGCTIMER_R;
+  
+  TIMER2_CTL_R = 0x00000000;    // 1) disable timer2A during setup
+  TIMER2_CFG_R = 0x00000000;    // 2) configure for 32-bit mode
+  TIMER2_TAMR_R = 0x00000001;   // 3) configure for one shot mode, default down-count settings
+  TIMER2_TAILR_R = period-1;    // 4) reload value
+  TIMER2_TAPR_R = 0;            // 5) bus clock resolution
+  TIMER2_ICR_R = 0x00000001;    // 6) clear timer2A timeout flag
+  TIMER2_IMR_R = 0x00000001;    // 7) arm timeout interrupt
+  NVIC_PRI5_R = (NVIC_PRI5_R&0x00FFFFFF)|0x80000000; // 8) priority 4
+// interrupts enabled in the main program after all devices initialized
+// vector number 39, interrupt number 23
+  NVIC_EN0_R = 1<<23;           // 9) enable IRQ 23 in NVIC
+  TIMER2_CTL_R = 0x00000001;    // 10) enable timer2A
+	EnableInterrupts();
+}
+void Timer2A_Handler(void){ 
+	
+  TIMER2_ICR_R = 0x00000001;   // acknowledge timer2A timeout
+  reset();
+}
+
+
